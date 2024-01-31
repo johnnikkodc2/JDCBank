f32cee71981ef1ddb71e9f896ed427e47a83edbe
johnnikkodc2 <johnnikko.delacruz.cics@ust.edu.ph>
2024-01-31 21:55:10 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,89 @@
 <template>
-  <div id="app" class="bpi-design">
-    <img src="./assets/img/bank logo.png" alt="Bank Logo" class="bank-logo" />
+  <div v-if="!showReceipt">
+    <div id="app" class="bpi-design">
+      <img src="./assets/img/bank logo.png" alt="Bank Logo" class="bank-logo" />
 
-    <p class="greeting">Hi, {{ accountHolder }}</p>
-    <div class="pin-display">
-      <span v-for="index in enteredPin.length" :key="index">*</span>
-    </div>
-    <div v-if="!isPinEntered" class="pin-entry">
-      <div class="numeric-keypad">
-        <button
-          v-for="digit in pinDigits"
-          :key="digit"
-          @click="enterDigit(digit)"
-          class="digit-button"
-        >
-          {{ digit }}
-        </button>
+      <p class="greeting">Hi, {{ accountHolder }}</p>
+
+      <div v-if="!isPinEntered" class="pin-entry">
+        <div class="pin-display">
+          <span v-for="index in enteredPin.length" :key="index">*</span>
+        </div>
+
+        <div class="numeric-keypad">
+          <button
+            v-for="digit in pinDigits"
+            :key="digit"
+            @click="enterDigit(digit)"
+            class="digit-button"
+          >
+            {{ digit }}
+          </button>
+        </div>
+
+        <button @click="clearPin" class="transaction-button">Clear</button>
+        <button @click="verifyPin" class="transaction-button">Submit</button>
       </div>
 
-      <button @click="clearPin" class="clear-button">Clear</button>
-      <button @click="verifyPin" class="submit-button">Submit</button>
-    </div>
+      <div v-else>
+        <div class="account-type">
+          <label for="accountType" class="account-type-label"
+            >Choose Account:</label
+          >
+          <select
+            v-model="selectedAccount"
+            @change="updateBalance"
+            class="account-type-dropdown"
+          >
+            <option value="savings">Savings Account</option>
+            <option value="checking">Checking Account</option>
+          </select>
+        </div>
 
-    <div v-if="isPinEntered">
-      <div class="account-type">
-        <label for="accountType" class="account-type-label"
-          >Choose Account:</label
-        >
-        <select
-          v-model="selectedAccount"
-          @change="updateBalance"
-          class="account-type-dropdown"
-        >
-          <option value="savings">Savings Account</option>
-          <option value="checking">Checking Account</option>
-        </select>
-      </div>
+        <div class="account-info">
+          <p class="account-holder">Account Holder: {{ accountHolder }}</p>
+          <p class="account-balance">
+            Account Balance: ₱{{ formattedBalance }}
+          </p>
+        </div>
 
-      <div class="account-info">
-        <p class="account-holder">Account Holder: {{ accountHolder }}</p>
-        <p class="account-balance">Account Balance: ₱{{ formattedBalance }}</p>
-      </div>
+        <div class="transaction-form">
+          <label for="amount" class="transaction-label">Amount:</label>
+          <input
+            type="number"
+            v-model="transactionAmount"
+            class="transaction-input"
+          />
 
-      <div class="transaction-form">
-        <label for="amount" class="transaction-label">Amount:</label>
-        <input
-          type="number"
-          v-model="transactionAmount"
-          class="transaction-input"
-        />
-        <button @click="deposit" class="transaction-button deposit">
-          Deposit
-        </button>
-        <button @click="withdraw" class="transaction-button withdraw">
-          Withdraw
-        </button>
-        <button @click="reset" class="transaction-button reset">Reset</button>
-        <button
-          @click="generateReceipt"
-          class="transaction-button generate-receipt"
-        >
-          Generate Receipt
-        </button>
-        <button @click="logout" class="transaction-button logout">
-          Logout
-        </button>
-        <button
-          @click="newTransaction"
-          class="transaction-button new-transaction"
-        >
-          New Transaction
-        </button>
-      </div>
-
-      <div v-if="transactionHistory.length > 0" class="receipt">
-        <h2>Bank Receipt</h2>
-        <ul>
-          <li v-for="(transaction, index) in transactionHistory" :key="index">
-            {{ transaction.type }}: ₱{{ transaction.amount }}
-          </li>
-        </ul>
+          <button @click="deposit" class="transaction-button">Deposit</button>
+          <button @click="withdraw" class="transaction-button">Withdraw</button>
+          <button @click="logout" class="transaction-button">Logout</button>
+        </div>
       </div>
     </div>
   </div>
-  <audio ref="clickSound" src="./assets/audio/pinbeep.mp3"></audio>
+  <div v-if="showReceipt">
+    <Receipt
+      :accountHolder="accountHolder"
+      :selectedAccount="selectedAccount"
+      :formattedBalance="formattedBalance"
+      :transactionHistory="transactionHistory"
+    />
+  </div>
 </template>
 
 <script>
+import Receipt from "./Receipt.vue";
 export default {
+  components: {
+    Receipt,
+  },
   data() {
     return {
       bankName: "BPI",
       accountHolder: "John",
       selectedAccount: "savings",
+      showReceipt: false,
       balance: {
         savings: 1000,
         checking: 5000,
@@ -111,15 +102,13 @@ export default {
     },
   },
   methods: {
-    playClickSound() {
-      this.$refs.clickSound.play();
-    },
     deposit() {
       if (this.transactionAmount > 0) {
         this.balance[this.selectedAccount] += parseFloat(
           this.transactionAmount
         );
         this.recordTransaction("Deposit", this.transactionAmount);
+        this.showReceipt = true;
       }
     },
     withdraw() {
@@ -131,6 +120,8 @@ export default {
           this.transactionAmount
         );
         this.recordTransaction("Withdrawal", this.transactionAmount);
+        this.showReceipt = true;
+        this.generateReceipt();
       } else {
         alert("Insufficient funds!");
       }
@@ -138,11 +129,7 @@ export default {
     reset() {
       this.transactionAmount = 0;
     },
-    updateBalance() {
-      // Add any additional logic if needed when the account type changes
-    },
     enterDigit(digit) {
-      this.playClickSound();
       if (this.enteredPin.length < 4) {
         this.enteredPin += digit.toString();
       }
@@ -161,21 +148,24 @@ export default {
     logout() {
       this.isPinEntered = false;
       this.clearPin();
-      this.transactionHistory = []; // Clear transaction history on logout
+      this.transactionHistory = [];
     },
     newTransaction() {
       this.transactionAmount = 0;
       this.transactionHistory = [];
     },
     generateReceipt() {
-      // This function can be used to generate a receipt based on the current transaction history
-      // In this example, we are simply logging the receipt to the console
       console.log("Bank Receipt:");
       this.transactionHistory.forEach((transaction, index) => {
         console.log(
           `${index + 1}. ${transaction.type}: ₱${transaction.amount}`
         );
       });
+
+      // Reset values after generating the receipt
+      this.selectedAccount = ""; // Clear the selected account
+      this.transactionAmount = 0; // Reset transaction amount
+      // Add more data reset as needed
     },
     recordTransaction(type, amount) {
       this.transactionHistory.push({
---
+++ src/Receipt.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="bpi-design">
+    <img src="./assets/img/bank logo.png" alt="Bank Logo" class="bank-logo" />
+
+    <div class="receipt">
+      <h2>Bank Receipt</h2>
+      <p>Name: {{ accountHolder }}</p>
+      <p>Date: {{ getCurrentDate() }}</p>
+      <p>Time: {{ getCurrentTime() }}</p>
+      <p>
+        Account Type:
+        {{
+          selectedAccount === "savings" ? "Savings Account" : "Checking Account"
+        }}
+      </p>
+      <p>Balance: ₱{{ formattedBalance }}</p>
+      <ul>
+        <li v-for="(transaction, index) in transactionHistory" :key="index">
+          {{ transaction.type }}: ₱{{ transaction.amount }}
+        </li>
+      </ul>
+      <button @click="printReceipt" class="print-button">Print Receipt</button>
+
+      <button @click="makeAnotherTransaction" class="transaction-button2">
+        Make Another Transaction
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    accountHolder: {
+      type: String,
+      default: "John",
+    },
+    selectedAccount: {
+      type: String,
+      default: "savings",
+    },
+    formattedBalance: {
+      type: String,
+      default: "0",
+    },
+    transactionHistory: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    getCurrentDate() {
+      const now = new Date();
+      return now.toLocaleDateString();
+    },
+    getCurrentTime() {
+      const now = new Date();
+      return now.toLocaleTimeString();
+    },
+    printReceipt() {
+      window.print();
+    },
+    makeAnotherTransaction() {
+      window.location.reload();
+    },
+  },
+};
+</script>
+
+<style>
+.print-button,
+.transaction-button2 {
+  margin-top: 10px;
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
